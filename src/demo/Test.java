ab7d9067807df61b40af31a755adf681d8ecb0ce
wonderpan <wonderpan@outlook.com>
2018-10-18 16:47:38 +0800

    first

--- src/demo/Test.java
+++ src/demo/Test.java
@@ -1,9 +1,42 @@
 package demo;
 
 import java.nio.CharBuffer;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Test {
     public static void mian(String[] args){
+        int aHundredMillion = 10000;
 
+        Map<Integer, Integer> map = new HashMap<>();
+
+        long s1 = System.currentTimeMillis();
+        for (int i = 0; i < aHundredMillion; i++) {
+            map.put(i, i);
+        }
+        long s2 = System.currentTimeMillis();
+
+        System.out.println("未初始化容量，耗时 ： " + (s2 - s1));
+
+
+        Map<Integer, Integer> map1 = new HashMap<>(aHundredMillion / 2);
+
+        long s5 = System.currentTimeMillis();
+        for (int i = 0; i < aHundredMillion; i++) {
+            map1.put(i, i);
+        }
+        long s6 = System.currentTimeMillis();
+
+        System.out.println("初始化容量"+aHundredMillion/2+"，耗时 ： " + (s6 - s5));
+
+
+        Map<Integer, Integer> map2 = new HashMap<>(aHundredMillion);
+
+        long s3 = System.currentTimeMillis();
+        for (int i = 0; i < aHundredMillion; i++) {
+            map2.put(i, i);
+        }
+        long s4 = System.currentTimeMillis();
+        System.out.println("初始化容量为"+aHundredMillion+"，耗时 ： " + (s4 - s3));
     }
 }
